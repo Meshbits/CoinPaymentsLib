1b180d802bc9bb6cb80fc5fa10aaa3add00aad75
Hanh <hanh425@gmail.com>
2021-05-13 14:48:23 +0800

    Initial commit

---
+++ CoinPaymentsLib/Interfaces.cs
@@ -0,0 +1,36 @@
+﻿namespace zcash.CoinPaymentsLib
+{
+  public enum ConfirmationSpeed
+    {
+        Slow, Normal, Fast
+    }
+
+    public interface PublicKeyPackage
+    {
+        string Address { get; }
+    }
+
+    public interface IOnlineCoinService
+    {
+        public delegate void AccountUpdateDelegate(string address, bool reorg);
+
+        bool ValidateAddress(string address);
+        decimal GetAddressBalance(string address, uint minConfirmations);
+        string PrepareUnsignedTx(string addressFrom, string addressTo, decimal amount, decimal fee);
+        string BroadcastSignedTx(string signedTx);
+        decimal EstimateFee(ConfirmationSpeed speed);
+
+        public void RegisterAccountUpdateListener(AccountUpdateDelegate listener);
+        void ImportPublicKeyPackage(PublicKeyPackage pubkey);
+
+        void Start();
+        void Stop();
+    }
+
+    public interface IOfflinelineCoinService
+    {
+        PublicKeyPackage generateAddress();
+
+        string SignTx(string unsignedTx);
+    }
+}
---
+++ CoinPaymentsLib/Stubs.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zcash.CoinPaymentsLib
+{
+    public class ZcashSaplingPublicKeyPackage : PublicKeyPackage
+    {
+        public string Address { get; private set; }
+
+        public ZcashSaplingPublicKeyPackage(string address)
+        {
+            this.Address = address;
+        }
+    }
+
+    public class ZcashOnlineServiceStub : IOnlineCoinService
+    {
+        public string BroadcastSignedTx(string signedTx)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal EstimateFee(ConfirmationSpeed speed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetAddressBalance(string address, uint minConfirmations)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImportPublicKeyPackage(PublicKeyPackage pubkey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PrepareUnsignedTx(string addressFrom, string addressTo, decimal amount, decimal fee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterAccountUpdateListener(IOnlineCoinService.AccountUpdateDelegate listener)
+        {
+            AccountUpdateListeners += listener;
+        }
+
+        public void ScanTransaction(byte[] rawTx)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateAddress(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IOnlineCoinService.AccountUpdateDelegate AccountUpdateListeners;
+    }
+
+    public class ZcashOfflineServiceStub : IOfflinelineCoinService
+    {
+        public PublicKeyPackage generateAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SignTx(string unsignedTx)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
